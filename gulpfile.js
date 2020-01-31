dd47357dc1dbe68276530a3bae2ec0cc19ab7ad0
M <marilynduf@gmail.com>
2020-01-30 20:02:13 -0500

    add day 3 + refactor html tags

--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,74 @@
 // Initialize modules
 // Importing specific gulp API functions lets us write them below as series() instead of gulp.series()
-const { src, dest, watch, series, parallel } = require('gulp');
-// Importing all the Gulp-related packages we want to use
+const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
-var replace = require('gulp-replace');
-var babel = require("gulp-babel");
-var browserSync = require('browser-sync').create();
+const replace = require('gulp-replace');
+const babel = require("gulp-babel");
+const browsersync = require('browser-sync').create();
 
-// File paths
-const files = {
-    scssPath: 'src/scss/**/*.scss',
-    jsPath: 'src/**/*.js',
-    htmlPath: 'src/**/*.html',
-    destination: 'dist'
-};
+var scssWatchSrc = 'src/**/*.scss';
+var scssMainSrc = 'src/scss/style.scss';
+var jsSrc = 'src/**/*.js';
+var htmlSrc = 'src/**/*.html';
+var dist = 'dist';
 
-// Sass task: compiles the style.scss file into style.css
-function scssTask(){
-    return src(files.scssPath)
-        .pipe(sourcemaps.init()) // initialize sourcemaps first
-        .pipe(sass().on('error', sass.logError)) // compile SCSS to CSS
-        .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
-        .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
-        // .pipe(replace(/(url()(..\/images\/\w+(.svg|.gif|.png|.jpg)))/gi, '$1../$2'))
-        .pipe(dest(files.destination)
-        ); // put final CSS in dist folder
-}
-// JS task: concatenates and uglifies JS files to script.js
 function jsTask(){
-    return src([
-        files.jsPath
-        //,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
-    ])
+    return gulp
+        .src(jsSrc)
         .pipe(babel())
         .pipe(concat('all.js'))
-        .pipe(dest('dist')
-        );
+        .pipe(gulp.dest('dist')
+    );
 }
 
-// Cachebust
-var cbString = new Date().getTime();
-function cacheBustTask(){
-    return src(['index.html'])
-        .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
-        .pipe(dest('.'));
+// // Cachebust
+// var cbString = new Date().getTime();
+// function cacheBustTask(){
+//     return src(['index.html'])
+//         .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
+//         .pipe(dest('.'));
+// }
+
+// BrowserSync Reload
+function reload() {
+    browsersync.reload();
+
 }
 
+// Sass task: compiles the style.scss file into style.css
+function scssTask() {
+    return gulp
+        .src(scssMainSrc)
+        .pipe(sourcemaps.init()) // initialize sourcemaps first
+        .pipe(sass()) // compile SCSS to CSS
+        .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
+        .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
+        .pipe(gulp.dest(dist))// put final CSS in dist folder
+        .pipe(browsersync.stream())
+}
 
 // Watch task: watch SCSS and JS files for changes
 // If any change, run scss and js tasks simultaneously
-function watchTask(){
-    browserSync.init({
-        server:{
-            baseDir: './'
-        }
+function watchTask() {
+    browsersync.init({
+        server: {
+            baseDir: "./"
+        },
     });
-    watch([files.scssPath, files.jsPath, files.htmlPath],
-        series(
-            parallel(scssTask, jsTask),
-            cacheBustTask
-        )
-    ).on('change', browserSync.reload)
+    gulp.watch(scssWatchSrc, scssTask);
+    gulp.watch(htmlSrc).on('change', reload);
+    gulp.watch(jsSrc, jsTask).on('change', reload);
 }
 
-// Export the default Gulp task so it can be run
-// Runs the scss and js tasks simultaneously
-// then runs cacheBust, then watch task
-exports.default = series(
-    parallel(scssTask, jsTask),
-    cacheBustTask,
-    watchTask
-);
+exports.watchTask = watchTask;
+exports.scssTask = scssTask;
+exports.jsTask = jsTask;
+
+var build = gulp.parallel(scssTask, jsTask, watchTask);
+
+gulp.task('default', build);
