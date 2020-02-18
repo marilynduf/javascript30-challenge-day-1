beaef6d8cc8ab55704471f98253b1eadb13ca6aa
M <marilynduf@gmail.com>
2020-02-17 19:57:59 -0500

    day-4 done: array methods

--- src/day-4/day-4.js
+++ src/day-4/day-4.js
@@ -6,73 +6,130 @@ const inventors = [
     {first: 'Johannes', last:'Kepler', year: 1571, passed:1630},
     {first: 'Max', last:'Planck', year: 1858, passed:1947},
 ];
+const cardio_arrays = ['filter()', 'sort()', 'map()', 'reduce()'];
 
-/***** filter() */
+let printArea = document.querySelector('#array_cardio');
 
-const fifteen = inventors.filter(inventor => inventor.year >= 1400 && inventor.year < 1600);
-//console.table(fifteen);
+const showSelectedArray = (name) => {
+
+    document.querySelector(".selected_array").textContent = '';
+
+    /* filter() */
+    if (name === 'filter()') {
+        const fifteen = inventors.filter(inventor => inventor.year >= 1400 && inventor.year < 1600);
+        fifteen.forEach(element => {
+            let display_test = document.querySelector(".selected_array");
+            const every_element = document.createElement("div");
+            every_element.textContent = element.first + ':' + element.year;
+            display_test.appendChild(every_element);
+        });
+    }
+    if (name === 'sort()') {
+        const orderBylastName = inventors.sort((a, b) => a.last > b.last ? 1 : -1);
+        orderBylastName.forEach(element => {
+            let display_test = document.querySelector(".selected_array");
+            const every_element = document.createElement("div");
+            every_element.textContent = element.first + ' ' + element.last;
+            display_test.appendChild(every_element);
+        });
+    }
+    if (name === 'map()') {
+        const fullNames = inventors.sort((a, b) => a.last > b.last ? 1 : -1);
+        fullNames.forEach(element => {
+            let display_test = document.querySelector(".selected_array");
+            const every_element = document.createElement("div");
+            every_element.textContent = element.value;
+            display_test.appendChild(every_element);
+        });
+    }
+    if (name === 'reduce()') {
+        const totalYears = inventors.reduce((total, inventor) =>
+        {
+            return total + (inventor.passed - inventor.year);
+        }, 0); // 0 est la valeur de déclaration de la variable total
+        const display_test = document.querySelector(".selected_array");
+        const every_element = document.createElement("div");
+        every_element.textContent = `${totalYears}`;
+        display_test.appendChild(every_element);
+
+    }
+
+};
 
 
- /***** map() */
+const dropdown = () => {
 
-//const fullNames = inventors.map(inventors => inventors.first + " " + inventors.last);
-const fullNames = inventors.map(inventors => `${inventors.first} ${inventors.last}`);
-//console.table(fullNames);
+    const component = document.createElement("div");
+
+    const input = createInput();
+    const dropdown = showDropdown();
+
+    component.appendChild(input);
+    component.appendChild(dropdown);
+    printArea.appendChild(component);
+};
+
+const createInput = () => {
+    // Creates the input outline
+    const input = document.createElement("div");
+    input.classList = "input";
+    input.addEventListener("click", toggleDropdown);
+
+    // Creates the input placeholder content
+    const inputPlaceholder = document.createElement("div");
+    inputPlaceholder.classList = "input__placeholder";
+
+    const placeholder = document.createElement("p");
+    placeholder.textContent = "Select methodes";
+    placeholder.classList.add('placeholder');
+
+    // Appends the placeholder and chevron (stored in assets.js)
+    inputPlaceholder.appendChild(placeholder);
+    //inputPlaceholder.appendChild(dropdownIcon());
+    input.appendChild(inputPlaceholder);
+
+    return input;
+};
+
+const showDropdown = () => {
+    const structure = document.createElement("div");
+    structure.classList.add("structure", "hide");
+
+    cardio_arrays.forEach(element => {
+        const { name } = element;
+        const option = document.createElement("div");
+        option.addEventListener("click", () => selectOption(element));
+        //option.setAttribute("id", id);
+
+        const n = document.createElement("h5");
+        n.textContent = element;
+
+        // const t = document.createElement("p");
+        // t.textContent = `(${title})`;
+
+        option.appendChild(n);
+        //option.appendChild(t);
+        structure.appendChild(option);
+    });
+    return structure;
+};
+
+const toggleDropdown = () => {
+    const dropdown = document.querySelector(".structure");
+    dropdown.classList.toggle("hide");
+
+    const input = document.querySelector(".input");
+    input.classList.toggle("input__active");
+};
+
+const selectOption = (name) => {
+    const text = document.querySelector('.placeholder');
+    text.textContent = name;
+    text.classList.add('input__selected');
+    toggleDropdown();
+    showSelectedArray(name);
 
 
-/***** sort() */
+};
 
-// const orderBylastName = inventors.sort(function (a, b) {
-//     // note: on doit comparer les majuscule avec les majuscule et les minuscules avec les minuscules.
-//     if (a.last > b.last){
-//         return 1
-//     }
-//     else return -1;
-// });
-// Arrow function (ES6)
-const orderBylastName = inventors.sort((a, b) => a.last > b.last ? 1 : -1);
-    // note: on doit comparer les majuscule avec les majuscule et les minuscules avec les minuscules.
-
-//console.table(orderBylastName);
-
-/***** map() */
-// La méthode map() crée un nouveau tableau avec les résultats de l'appel d'une fonction fournie sur chaque élément
-// du tableau appelant.
-
-
-/***** reduce() */
-// La méthode reduce() applique une fonction qui est un « accumulateur » et qui traite
-// chaque valeur d'une liste (de la gauche vers la droite) afin de la réduire à une seule valeur.
-// output: une seule valeur
-// Exercice: how many years did all the inventors live?
-
-const totalYears = inventors.reduce((total, inventor) =>
-{
-    return total + (inventor.passed - inventor.year);
-}, 0); // 0 est la valeur de déclaration de la variable total
-
-
-/***** exer. 7 sort() */
-// const pageWeb = 'https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris';
-// const category = document.querySelector('mw-category');
-// const links = Array.from(category.querySelectorAll('a'));
-// const de = links
-//     .map(link => links.textContent)
-//     .filter(streetName => streetName.includes('de'));
-
-// for (let i = 0; i < inventors.length; i++) {
-//     document.getElementById("array_cardio").innerHTML = inventors[i].first;
-// }
-// let app = document.querySelector('array_cardio');
-// app.appendChild(list);
-
-// Create an unordered list
-let list = document.getElementById('array_cardio');
-
-// Create a list item for each wizard
-// and append it to the list
-inventors.forEach(function (inventor) {
-    let li = document.createElement('li');
-    li.textContent = inventor.first;
-    list.appendChild(li);
-});
+dropdown();
